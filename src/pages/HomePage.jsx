6ddb32c2765cb00420416da31f69da6888055359
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-23 00:25:22 +0100

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import api from "../services/movieApi";
+import MoviesList from "../components/MoviesList";
+
+function HomePage() {
+    const [trendingMovies, setTrendingMovies] = useState([]);
+
+    useEffect(() => {
+        api.fetchTrending(setTrendingMovies);
+    }, []);
+    return (
+        <>
+        <h1>Trending today</h1>
+        <MoviesList movies={trendingMovies} />
+    </>
+    );
+}
+
+export default HomePage;
