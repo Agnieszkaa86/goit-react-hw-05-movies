e55e1ccd8b149312fe20f830214525673bbb243a
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-23 00:24:40 +0100

    Create Reviews.styled.js

---
+++ src/components/Reviews.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+export const StyledList = styled.ul`
+  padding: 0px;
+`;
+
+export const StyledItem = styled.li`
+  border: 1px solid black;
+  border-radius: 12px;
+  padding: 15px;
+  text-align: justify;
+  margin-bottom: 10px;
+  background-color: #878f99;
+  overflow: hidden;
+`;
