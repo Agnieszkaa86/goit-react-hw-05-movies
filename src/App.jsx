c1ba348dae1294b33a0902b191afa54e9bd26e37
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-23 00:27:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Movies from "./pages/Movies";
-import NotFound from "./pages/NotFound";
-import "./App.css";
+import { Routes, Route} from "react-router-dom";
+import React from 'react';
+import Home from "./components/Home";
 
-export function App() {
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const Movies = React.lazy(() => import("./pages/Movies"));
+const MovieDetailsPage = React.lazy(() => import("./pages/MovieDetailsPage"));
+const Cast= React.lazy(() => import("./components/Cast"));
+const Reviews = React.lazy(() => import("./components/Reviews"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+
+const  App = ()=> {
   return (
-    <div>
+    <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="notfound" element={<NotFound />} />
-        </Routes>
-    </div>
+        <Route path="/" element={<Home />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<Movies />}>
+            <Route path=":movieId" element={<MovieDetailsPage />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 export default App;
