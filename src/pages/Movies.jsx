a9657276a25ff3c0bf33086f626e5df9fd77c765
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-23 00:25:41 +0100

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,55 @@
-function Movies() {
-    return (<>
-    <h1>Movies</h1>
-    </>);
-}
+
+import MoviesList from "../components/MoviesList";
+import React, { useEffect, useState } from "react";
+import { Outlet, useParams, useSearchParams} from 'react-router-dom';
+import api from '../services/movieApi';
+import { Button, Input } from "./Movies.styled";
+
+
+
+const Movies = () => {
+  const [searchedMovie, setSearchedMovie] = useState([]);
+  const [searchQuery, setSearchQuery] = useSearchParams();
+  const query = searchQuery.get('query');
+  const searchMovie = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const formQuery = form.elements.query.value;
+    if (formQuery === '') {
+      return;
+    }
+    setSearchQuery({ query: formQuery });
+    form.reset();
+  };
+
+  useEffect(() => {
+    if (query === null) {
+      setSearchedMovie([]);
+      return;
+    }
+    api.fetchMovieByQuery(query, setSearchedMovie);
+  }, [query]);
+
+  const params = useParams();
+
+  return (
+    <>
+      {!params.movieId && (
+        <>
+          <form onSubmit={searchMovie}>
+            <Input type="text" name="query"/>
+            <Button type="submit">Search Movies</Button>
+          </form>
+          <MoviesList movies={searchedMovie} />
+        </>
+      )}
+      {params.movieId && (
+        <>
+          <Outlet />
+        </>
+      )}
+    </>
+  );
+};
 
 export default Movies;
