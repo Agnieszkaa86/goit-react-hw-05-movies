23b9730b14ae356537f2234fa3a7b1305ffcd3bd
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-23 00:26:16 +0100

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+import React from 'react';
+import { Main, Card, Header } from "./NotFound.styled";
+
 function NotFound() {
-    return (<>
-    <h1>NotFound</h1>
-    </>);
+    return (
+        <>
+        <Main>
+        <Card>
+          <Header>Page Not Found</Header>
+          <div>
+            <p>
+              Looks like you've followed a broken link or entered a URL that
+              doesn't exist on this site.
+            </p>
+            <Link to="/">Back to our site</Link> 
+          </div>
+        </Card>
+      </Main>
+        </>
+    );
 }
 
 export default NotFound;
