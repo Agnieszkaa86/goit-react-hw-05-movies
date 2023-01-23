c32a2fdb6c39885d8f0408988f012d4de0474ffe
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-23 17:42:18 +0100

    Update Movies.styled.js

--- src/pages/Movies.styled.js
+++ src/pages/Movies.styled.js
@@ -4,12 +4,14 @@ export const Button = styled.button`
   margin-left: 5px;
   align-items: center;
   background-color: #fff;
-  border-radius: 12px;
+  border-radius: 2px;
   box-shadow: transparent 0 0 0 3px, rgba(18, 18, 18, 0.1) 0 6px 20px;
   box-sizing: border-box;
   color: #121212;
   cursor: pointer;
   display: inline-flex;
+  vertical-align: middle; 
+  vertical-align: text-bottom;
   flex: 1 1 auto;
   font-family: Inter, sans-serif;
   font-size: 1rem;
@@ -17,7 +19,7 @@ export const Button = styled.button`
   justify-content: center;
   line-height: 1;
   outline: none;
-  padding: 1rem 1.2rem;
+  padding: 1.1rem 1.2rem;
   text-align: center;
   text-decoration: none;
   transition: box-shadow 0.2s, -webkit-box-shadow 0.2s;
@@ -27,12 +29,15 @@ export const Button = styled.button`
   -webkit-user-select: none;
   touch-action: manipulation;
   &:hover {
-    box-shadow: #121212 0 0 0 3px, transparent 0 0 0 0;
+    box-shadow: #121212 0 0 0 2px, transparent 0 0 0 0;
   }
 `;
 
 export const Input = styled.input`
-  font-size: 1rem;
-  border-radius: 12px;
-  padding: 0 1.2rem;
+  font-size: 2rem;
+  border-radius: 3px;
+  padding: 0.4rem 0.5rem;
+  vertical-align: middle; 
+  vertical-align: text-bottom;
+
 `;
