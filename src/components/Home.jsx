619564107ce4d7699f563b7e71b4f6924eaba732
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-23 17:08:48 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,21 @@
-import { Outlet} from "react-router-dom";
-import { Suspense } from "react";
-import Loader from "./Loader";
-import { Nav, StyledLink, Wrapper } from "./Home.styled";
-
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+import Loader from './Loader';
+import { Nav, StyledLink, Wrapper } from './Home.styled';
 
 const Home = () => {
-    return (
-        <>
-            <Nav>
-             
-                <StyledLink to="/">Home</StyledLink>
-                <StyledLink to="/movies">Movies</StyledLink>
-        
-            </Nav>
-            <Suspense fallback={<Loader />}>
-            <Wrapper>
-            <Outlet />
-            </Wrapper>
-            </Suspense>
-            
-        </>
-    )
-}
+  return (
+    <>
+      <Nav>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/movies">Movies</StyledLink>
+      </Nav>
+      <Suspense fallback={<Loader />}>
+        <Wrapper>
+          <Outlet />
+        </Wrapper>
+      </Suspense>
+    </>
+  );
+};
 export default Home;
