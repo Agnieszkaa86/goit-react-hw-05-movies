9a41816e067e908515bd1fa052704e3044f64364
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-23 17:08:38 +0100

    Update MoviesList.jsx

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Link, useLocation } from "react-router-dom";
-import PropTypes from "prop-types";
+import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const MoviesList = ({movies}) => {
-    const location = useLocation();
+const MoviesList = ({ movies }) => {
+  const location = useLocation();
 
-    return (
-        <>
-            <ul>
-                {movies.map(({ id, title }) => (
-                    <li key={id}>
-                        <Link to={`/movies/${id}`} state={{ from: location }}>
-                            {title}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+  return (
+    <>
+      <ul>
+        {movies.map(({ id, title }) => (
+          <li key={id}>
+            <Link to={`/movies/${id}`} state={{ from: location }}>
+              {title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 MoviesList.propTypes = {
-    movies: PropTypes.arrayOf(PropTypes.object),
+  movies: PropTypes.arrayOf(PropTypes.object),
 };
 export default MoviesList;
