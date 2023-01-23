93ac2aa48d68df9c0c286a9cfd9212e085699d3f
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-23 17:42:23 +0100

    Update MovieDetailsPage.styled.js

--- src/pages/MovieDetailsPage.styled.js
+++ src/pages/MovieDetailsPage.styled.js
@@ -4,7 +4,7 @@ export const Button = styled.button`
   margin-bottom: 25px;
   align-items: center;
   background-color: #fff;
-  border-radius: 12px;
+  border-radius: 2px;
   box-shadow: transparent 0 0 0 3px, rgba(18, 18, 18, 0.1) 0 6px 20px;
   box-sizing: border-box;
   color: #121212;
@@ -15,18 +15,15 @@ export const Button = styled.button`
   font-size: 1.2rem;
   font-weight: 700;
   justify-content: center;
-  line-height: 1;
   outline: none;
-  padding: 1rem 1.2rem;
+  padding: 0.5rem 0.5rem;
   text-align: center;
   text-decoration: none;
   transition: box-shadow 0.2s, -webkit-box-shadow 0.2s;
-  white-space: nowrap;
-  border: 0;
   user-select: none;
   -webkit-user-select: none;
   touch-action: manipulation;
   &:hover {
-    box-shadow: #121212 0 0 0 3px, transparent 0 0 0 0;
+    box-shadow: #FF0000 0 0 0 3px, transparent 0 0 0 0;
   }
 `;
