06794c4c8a020124dc7a02819a004015a013095e
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-25 18:58:57 +0100

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../services/movieApi';
+import PropTypes from 'prop-types';
 import { StyledList, StyledItem } from './Reviews.styled';
 
 const Reviews = () => {
@@ -30,4 +31,13 @@ const Reviews = () => {
   );
 };
 
+Reviews.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      author: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 export default Reviews;
